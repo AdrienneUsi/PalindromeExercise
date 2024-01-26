bdbb6738d1865409bc900e948bd70a3c8c940ba5
AdrienneUsi <usiadrienne35@gmail.com>
2024-01-26 11:51:20 -0800

    did exercise

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,23 @@
-using System;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("civic", true)]
+        [InlineData("tattarrattat", true)]
+        [InlineData("hey", false)]
+        public void PalindromeTest(string word, bool expected)
         {
+            //Arrange
+            var wordSmith = new PalindromeExercise.WordSmith();
 
+            //Act
+            var actual = wordSmith.IsAPalindrome(word);
+
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
